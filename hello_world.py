d4d0e1169eff6ed91577a90b5f83a14d19fbf0cd
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-06-07 20:32:19 +0800

    variables and user input

--- hello_world.py
+++ hello_world.py
@@ -9,3 +9,7 @@ print(f"Today is: {day} in the {week} week of {month}")
 # f= function
 #task = create a varaible that represnets the month in the same sentence
 
+run1_dist = 1400
+print(run1_dist)
+print(type(run1_dist))
+
---
+++ user_input.py
@@ -0,0 +1,16 @@
+# name = input("what is your name?")
+# print(f"Hi {name}")
+
+# age = input(f"Hi {name}, how old are you?")
+# years_until_100 = 100 - int(age)
+# print(f"wow {name}! You'll \nbe 100 in {years_until_100} years")
+
+# number = input("Enter a number: ")
+# number2 = input(f"Enter another number: ")
+# Answer = float(number) + float(number2)
+# print(f"The answer is {Answer}")
+
+number = input("Enter a number: ")
+number2 = input(f"Enter another number: ")
+Answer = float(number) * float(number2)
+print(f"The answer is {Answer}")
