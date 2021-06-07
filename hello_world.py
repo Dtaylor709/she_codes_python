eda0fe5523963724505379aa15e8490211c6b542
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-06-07 19:51:52 +0800

    Create hello_world.py
    intro to variables

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("Hello, Djuarli!")
+
+day = "Monday"
+month = "June"
+week= "second"
+
+print(f"Today is: {day} in the {week} week of {month}")
+
+# f= function
+#task = create a varaible that represnets the month in the same sentence
+
